2c2e258ec2efb6944a112ee2d9e82ac25303e27a
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-28 20:56:04 -0600

    Add Timber dependency

--- app/src/main/java/com/alexiscrack3/spinny/SpinnyApplication.kt
+++ app/src/main/java/com/alexiscrack3/spinny/SpinnyApplication.kt
@@ -1,9 +1,13 @@
 package com.alexiscrack3.spinny
 
 import androidx.multidex.MultiDexApplication
+import timber.log.Timber
 
 class SpinnyApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
