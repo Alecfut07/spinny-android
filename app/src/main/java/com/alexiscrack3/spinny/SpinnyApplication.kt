dd461970bb35254fdfefdba35985b75b56b9133e
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-28 20:52:25 -0600

    Create custom application class and enable multidex

---
+++ app/src/main/java/com/alexiscrack3/spinny/SpinnyApplication.kt
@@ -0,0 +1,9 @@
+package com.alexiscrack3.spinny
+
+import androidx.multidex.MultiDexApplication
+
+class SpinnyApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
