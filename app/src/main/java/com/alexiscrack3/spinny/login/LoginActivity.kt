24f08dd9600049dd6b52200b0e060201f8af1a78
Alexis Ortega <alexis.oc3@gmail.com>
2020-05-28 18:49:04 -0600

    Create login screen

--- app/src/main/java/com/alexiscrack3/spinny/login/LoginActivity.kt
+++ app/src/main/java/com/alexiscrack3/spinny/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.alexiscrack3.spinny
+package com.alexiscrack3.spinny.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alexiscrack3.spinny.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
